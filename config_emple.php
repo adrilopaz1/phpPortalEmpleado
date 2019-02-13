a7a6e590ab650c6c38367f9a52e56de65fff247a
adrilopaz1 <47173769+adrilopaz1@users.noreply.github.com>
2019-02-13 12:24:45 +0100

    Update config_emple.php

--- config_emple.php
+++ config_emple.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', '10.129.44.22');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'rootroot');
    define('DB_DATABASE', 'employees');
